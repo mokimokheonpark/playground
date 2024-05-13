3ce57ecc6abdb64e52188d8a46fdcf78c4259c7a
Mokheon Park <mokimokheonpark@gmail.com>
2024-05-13 15:20:06 -0400

    created additional basic tests

--- app/coin/page.test.tsx
+++ app/coin/page.test.tsx
@@ -2,6 +2,10 @@ import { render } from "@testing-library/react";
 import CoinPage from "./page";
 
 describe("Coin Page", () => {
+  test("renders without crashing", () => {
+    render(<CoinPage />);
+  });
+
   test("renders header", () => {
     const { getByText } = render(<CoinPage />);
     const headerElement = getByText("Coin Flipping");
--- app/page.test.tsx
+++ app/page.test.tsx
@@ -2,6 +2,10 @@ import { render } from "@testing-library/react";
 import Home from "./page";
 
 describe("Home Page", () => {
+  test("renders without crashing", () => {
+    render(<Home />);
+  });
+
   test("renders header", () => {
     const { getByText } = render(<Home />);
     const headerElement = getByText("Welcome to Playground!");
