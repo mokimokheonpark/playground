232b3d4ba8a32833d03bdc120cc7d63ee5c5b3ed
Mokheon Park <mokimokheonpark@gmail.com>
2024-05-13 16:48:44 -0400

    implemented bet-ammount-input

--- app/components/Coin.test.tsx
+++ app/components/Coin.test.tsx
@@ -1,4 +1,4 @@
-import { render, fireEvent } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import Coin from "./Coin";
 
 describe("Coin Component", () => {
@@ -11,10 +11,11 @@ describe("Coin Component", () => {
     expect(getByText("Points: 10000")).toBeInTheDocument();
   });
 
-  test("updates points when Head-button is clicked", () => {
-    const { getByText, getByTestId } = render(<Coin />);
+  test("updates points correctly when choosing Head", () => {
+    const { getByRole, getByTestId, getByText } = render(<Coin />);
     const points = getByTestId("Points");
     const headButton = getByText("Head");
+    fireEvent.change(getByRole("spinbutton"), { target: { value: "100" } });
     fireEvent.click(headButton);
     let updatedPoints;
     if (points.textContent) {
@@ -23,15 +24,40 @@ describe("Coin Component", () => {
     expect([9900, 10097]).toContainEqual(updatedPoints);
   });
 
-  test("updates points when Tail-button is clicked", () => {
-    const { getByText, getByTestId } = render(<Coin />);
+  test("updates points correctly when choosing Tail", () => {
+    const { getByRole, getByTestId, getByText } = render(<Coin />);
     const points = getByTestId("Points");
     const tailButton = getByText("Tail");
+    fireEvent.change(getByRole("spinbutton"), { target: { value: "1200" } });
     fireEvent.click(tailButton);
     let updatedPoints;
     if (points.textContent) {
       updatedPoints = parseInt(points.textContent.split(" ")[1]);
     }
-    expect([9900, 10097]).toContainEqual(updatedPoints);
+    expect([8800, 11164]).toContainEqual(updatedPoints);
+  });
+
+  test("does not update points if bet amount is 0", () => {
+    const { getByRole, getByText } = render(<Coin />);
+    const headButton = getByRole("button", { name: "Head" });
+    fireEvent.change(getByRole("spinbutton"), { target: { value: "0" } });
+    fireEvent.click(headButton);
+    expect(getByText("Points: 10000")).toBeInTheDocument();
+  });
+
+  test("does not update points if bet amount is not a multiple of 100", () => {
+    const { getByRole, getByText } = render(<Coin />);
+    const headButton = getByRole("button", { name: "Head" });
+    fireEvent.change(getByRole("spinbutton"), { target: { value: "150" } });
+    fireEvent.click(headButton);
+    expect(getByText("Points: 10000")).toBeInTheDocument();
+  });
+
+  test("does not update points if bet amount is greater than current points", () => {
+    const { getByRole, getByText } = render(<Coin />);
+    const headButton = getByRole("button", { name: "Head" });
+    fireEvent.change(getByRole("spinbutton"), { target: { value: "11000" } });
+    fireEvent.click(headButton);
+    expect(getByText("Points: 10000")).toBeInTheDocument();
   });
 });
--- app/components/Coin.tsx
+++ app/components/Coin.tsx
@@ -4,30 +4,62 @@ import { useState } from "react";
 
 export default function Coin() {
   const [points, setPoints] = useState<number>(10000);
+  const [betAmountInput, setBetAmountInput] = useState<number>(100);
 
   const handleChooseHead = (): void => {
     const result: number = Math.floor(Math.random() * 2) + 1;
     if (result === 1) {
-      setPoints((prev) => prev + 97);
+      setPoints((prev) => prev + betAmountInput * 0.97);
     } else {
-      setPoints((prev) => prev - 100);
+      setPoints((prev) => prev - betAmountInput);
     }
   };
 
   const handleChooseTail = (): void => {
     const result: number = Math.floor(Math.random() * 2) + 1;
     if (result === 2) {
-      setPoints((prev) => prev + 97);
+      setPoints((prev) => prev + betAmountInput * 0.97);
     } else {
-      setPoints((prev) => prev - 100);
+      setPoints((prev) => prev - betAmountInput);
     }
   };
 
   return (
     <div data-testid={"Coin-Component"}>
       <h4 data-testid={"Points"}>Points: {points}</h4>
-      <button onClick={handleChooseHead}>Head</button>
-      <button onClick={handleChooseTail}>Tail</button>
+      <input
+        data-testid={"Bet-Amount"}
+        className="mb-20"
+        type="number"
+        min={100}
+        step={100}
+        required
+        value={betAmountInput}
+        onChange={(e) => {
+          const value: number = parseInt(e.target.value);
+          setBetAmountInput(value);
+        }}
+      />
+      <button
+        onClick={handleChooseHead}
+        disabled={
+          betAmountInput === 0 ||
+          betAmountInput % 100 !== 0 ||
+          betAmountInput > points
+        }
+      >
+        Head
+      </button>
+      <button
+        onClick={handleChooseTail}
+        disabled={
+          betAmountInput === 0 ||
+          betAmountInput % 100 !== 0 ||
+          betAmountInput > points
+        }
+      >
+        Tail
+      </button>
     </div>
   );
 }
