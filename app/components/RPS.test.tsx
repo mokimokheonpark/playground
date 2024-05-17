2a116a136a8f4ae10904eb529e77a099ff3858aa
Mokheon Park <mokimokheonpark@gmail.com>
2024-05-16 23:35:25 -0400

    implemented Rock-Paper-Scissors component

--- app/components/RPS.test.tsx
+++ app/components/RPS.test.tsx
@@ -1,8 +1,250 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import RPS from "./RPS";
 
 describe("Rock-Paper-Scissors Component", () => {
   test("renders without crashing", () => {
     render(<RPS />);
+  });
+
+  test("displays the initial points correctly", () => {
+    const { getByText } = render(<RPS />);
+    expect(getByText("Points: 10000")).toBeInTheDocument();
+  });
+
+  test("updates points correctly when choosing Rock", () => {
+    const { getByRole, getByTestId } = render(<RPS />);
+    const points = getByTestId("Points");
+    const rockButton = getByRole("button", { name: "Rock" });
+    fireEvent.change(getByTestId("Bet-Amount"), { target: { value: "100" } });
+    fireEvent.click(rockButton);
+    let updatedPoints;
+    if (points.textContent) {
+      updatedPoints = parseInt(points.textContent.split(" ")[1]);
+    }
+    expect([9900, 10195]).toContainEqual(updatedPoints);
+  });
+
+  test("updates points correctly when choosing Paper", () => {
+    const { getByRole, getByTestId } = render(<RPS />);
+    const points = getByTestId("Points");
+    const paperButton = getByRole("button", { name: "Paper" });
+    fireEvent.change(getByTestId("Bet-Amount"), { target: { value: "200" } });
+    fireEvent.click(paperButton);
+    let updatedPoints;
+    if (points.textContent) {
+      updatedPoints = parseInt(points.textContent.split(" ")[1]);
+    }
+    expect([9800, 10390]).toContainEqual(updatedPoints);
+  });
+
+  test("updates points correctly when choosing Scissors", () => {
+    const { getByRole, getByTestId } = render(<RPS />);
+    const points = getByTestId("Points");
+    const scissorsButton = getByRole("button", { name: "Scissors" });
+    fireEvent.change(getByTestId("Bet-Amount"), { target: { value: "500" } });
+    fireEvent.click(scissorsButton);
+    let updatedPoints;
+    if (points.textContent) {
+      updatedPoints = parseInt(points.textContent.split(" ")[1]);
+    }
+    expect([9500, 10975]).toContainEqual(updatedPoints);
+  });
+
+  test("does not update points if bet amount is 0", () => {
+    const { getByRole, getByTestId, getByText } = render(<RPS />);
+    const rockButton = getByRole("button", { name: "Rock" });
+    fireEvent.change(getByTestId("Bet-Amount"), { target: { value: "0" } });
+    fireEvent.click(rockButton);
+    expect(getByText("Points: 10000")).toBeInTheDocument();
+  });
+
+  test("does not update points if bet amount is not a multiple of 100", () => {
+    const { getByRole, getByTestId, getByText } = render(<RPS />);
+    const paperButton = getByRole("button", { name: "Paper" });
+    fireEvent.change(getByTestId("Bet-Amount"), { target: { value: "90" } });
+    fireEvent.click(paperButton);
+    expect(getByText("Points: 10000")).toBeInTheDocument();
+  });
+
+  test("does not update points if bet amount is greater than current points", () => {
+    const { getByRole, getByTestId, getByText } = render(<RPS />);
+    const scissorsButton = getByRole("button", { name: "Scissors" });
+    fireEvent.change(getByTestId("Bet-Amount"), { target: { value: "50000" } });
+    fireEvent.click(scissorsButton);
+    expect(getByText("Points: 10000")).toBeInTheDocument();
+  });
+
+  test("displays correct player-choice when choosing Rock", () => {
+    const { getByRole, getByTestId } = render(<RPS />);
+    const rockButton = getByRole("button", { name: "Rock" });
+    fireEvent.click(rockButton);
+    const choice = getByTestId("Choice");
+    expect(choice).toHaveTextContent("Rock");
+  });
+
+  test("displays correct player-choice when choosing Paper", () => {
+    const { getByRole, getByTestId } = render(<RPS />);
+    const paperButton = getByRole("button", { name: "Paper" });
+    fireEvent.click(paperButton);
+    const choice = getByTestId("Choice");
+    expect(choice).toHaveTextContent("Paper");
+  });
+
+  test("displays correct player-choice when choosing Scissors", () => {
+    const { getByRole, getByTestId } = render(<RPS />);
+    const scissorsButton = getByRole("button", { name: "Scissors" });
+    fireEvent.click(scissorsButton);
+    const choice = getByTestId("Choice");
+    expect(choice).toHaveTextContent("Scissors");
+  });
+
+  test("displays correct game-result-message when player wins/draws/loses with Rock", () => {
+    const { getByRole, getByTestId, getByText } = render(<RPS />);
+    const rockButton = getByRole("button", { name: "Rock" });
+    fireEvent.change(getByTestId("Bet-Amount"), { target: { value: "1000" } });
+    fireEvent.click(rockButton);
+    const enemyResult = getByTestId("Enemy-Result");
+    if (enemyResult.textContent === "Scissors") {
+      expect(getByText("Win! You earned 1950 points!")).toBeInTheDocument();
+    } else if (enemyResult.textContent === "Rock") {
+      expect(getByText("Draw! You lost 1000 points...")).toBeInTheDocument();
+    } else {
+      expect(getByText("Lose! You lost 1000 points...")).toBeInTheDocument();
+    }
+  });
+
+  test("displays correct game-result-message when player wins/draws/loses with Paper", () => {
+    const { getByRole, getByTestId, getByText } = render(<RPS />);
+    const paperButton = getByRole("button", { name: "Paper" });
+    fireEvent.change(getByTestId("Bet-Amount"), { target: { value: "1700" } });
+    fireEvent.click(paperButton);
+    const enemyResult = getByTestId("Enemy-Result");
+    if (enemyResult.textContent === "Rock") {
+      expect(getByText("Win! You earned 3315 points!")).toBeInTheDocument();
+    } else if (enemyResult.textContent === "Paper") {
+      expect(getByText("Draw! You lost 1700 points...")).toBeInTheDocument();
+    } else {
+      expect(getByText("Lose! You lost 1700 points...")).toBeInTheDocument();
+    }
+  });
+
+  test("displays correct game-result-message when player wins/draws/loses with Scissors", () => {
+    const { getByRole, getByTestId, getByText } = render(<RPS />);
+    const scissorsButton = getByRole("button", { name: "Scissors" });
+    fireEvent.change(getByTestId("Bet-Amount"), { target: { value: "600" } });
+    fireEvent.click(scissorsButton);
+    const enemyResult = getByTestId("Enemy-Result");
+    if (enemyResult.textContent === "Paper") {
+      expect(getByText("Win! You earned 1170 points!")).toBeInTheDocument();
+    } else if (enemyResult.textContent === "Scissors") {
+      expect(getByText("Draw! You lost 600 points...")).toBeInTheDocument();
+    } else {
+      expect(getByText("Lose! You lost 600 points...")).toBeInTheDocument();
+    }
+  });
+
+  test("displays the number of total played games correctly", () => {
+    const { getByRole, getByTestId } = render(<RPS />);
+    const rockButton = getByRole("button", { name: "Rock" });
+    const paperButton = getByRole("button", { name: "Paper" });
+    const scissorsButton = getByRole("button", { name: "Scissors" });
+    for (let i = 0; i < 8; i++) {
+      fireEvent.click(scissorsButton);
+      fireEvent.click(rockButton);
+      fireEvent.click(paperButton);
+    }
+    const playCount = getByTestId("Play-Count");
+    let playCountNumber;
+    if (playCount.textContent) {
+      playCountNumber = parseInt(playCount.textContent.split(" ")[1]);
+    }
+    expect(playCountNumber).toEqual(24);
+  });
+
+  test("displays the number of total won/drawn/lost games correctly", () => {
+    const { getByRole, getByTestId } = render(<RPS />);
+    const paperButton = getByRole("button", { name: "Paper" });
+    fireEvent.click(paperButton);
+    const winCount = getByTestId("Win-Count");
+    let winCountNumber;
+    if (winCount.textContent) {
+      winCountNumber = parseInt(winCount.textContent.split(" ")[1]);
+    }
+    const drawCount = getByTestId("Draw-Count");
+    let drawCountNumber;
+    if (drawCount.textContent) {
+      drawCountNumber = parseInt(drawCount.textContent.split(" ")[1]);
+    }
+    const lossCount = getByTestId("Loss-Count");
+    let lossCountNumber;
+    if (lossCount.textContent) {
+      lossCountNumber = parseInt(lossCount.textContent.split(" ")[1]);
+    }
+    const enemyResult = getByTestId("Enemy-Result");
+    if (enemyResult.textContent === "Rock") {
+      expect(winCountNumber).toEqual(1);
+      expect(drawCountNumber).toEqual(0);
+      expect(lossCountNumber).toEqual(0);
+    } else if (enemyResult.textContent === "Paper") {
+      expect(winCountNumber).toEqual(0);
+      expect(drawCountNumber).toEqual(1);
+      expect(lossCountNumber).toEqual(0);
+    } else {
+      expect(winCountNumber).toEqual(0);
+      expect(drawCountNumber).toEqual(0);
+      expect(lossCountNumber).toEqual(1);
+    }
+  });
+
+  test("displays the number of total Rock/Paper/Scissors correctly", () => {
+    const { getByRole, getByTestId } = render(<RPS />);
+    const scissorsButton = getByRole("button", { name: "Scissors" });
+    fireEvent.click(scissorsButton);
+    const rockCount = getByTestId("Rock-Count");
+    let rockCountNumber;
+    if (rockCount.textContent) {
+      rockCountNumber = parseInt(rockCount.textContent.split(" ")[1]);
+    }
+    const paperCount = getByTestId("Paper-Count");
+    let paperCountNumber;
+    if (paperCount.textContent) {
+      paperCountNumber = parseInt(paperCount.textContent.split(" ")[1]);
+    }
+    const scissorsCount = getByTestId("Scissors-Count");
+    let scissorsCountNumber;
+    if (scissorsCount.textContent) {
+      scissorsCountNumber = parseInt(scissorsCount.textContent.split(" ")[1]);
+    }
+    const enemyResult = getByTestId("Enemy-Result");
+    if (enemyResult.textContent === "Rock") {
+      expect(rockCountNumber).toEqual(1);
+      expect(paperCountNumber).toEqual(0);
+      expect(scissorsCountNumber).toEqual(0);
+    } else {
+      expect(rockCountNumber).toEqual(0);
+    }
+  });
+
+  test("displays past-enemy-results correctly", () => {
+    const { getByRole, getByTestId } = render(<RPS />);
+    const rockButton = getByRole("button", { name: "Rock" });
+    const paperButton = getByRole("button", { name: "Paper" });
+    const scissorsButton = getByRole("button", { name: "Scissors" });
+    fireEvent.click(rockButton);
+    fireEvent.click(paperButton);
+    fireEvent.click(scissorsButton);
+    const pastEnemyResultsContainer = getByTestId("Past-Enemy-Results");
+    const pastEnemyResults =
+      pastEnemyResultsContainer.textContent?.split(": ")[1];
+    const possibleResults = [];
+    const rps = ["R", "P", "S"];
+    for (let i = 0; i < rps.length; i++) {
+      for (let j = 0; j < rps.length; j++) {
+        for (let k = 0; k < rps.length; k++) {
+          possibleResults.push(rps[i] + " " + rps[j] + " " + rps[k] + " ");
+        }
+      }
+    }
+    expect(possibleResults).toContain(pastEnemyResults);
   });
 });
--- app/components/RPS.tsx
+++ app/components/RPS.tsx
@@ -1,3 +1,265 @@
+"use client";
+
+import { useState } from "react";
+
 export default function RPS() {
-  return <div data-testid={"RPS-Component"}>RPS Component</div>;
+  type RockPaperScissors = "Rock" | "Paper" | "Scissors";
+  type RPS = "R" | "P" | "S";
+  type RPSArray = RPS[];
+
+  const [points, setPoints] = useState<number>(10000);
+  const [betAmountInput, setBetAmountInput] = useState<number>(100);
+  const [betAmount, setBetAmount] = useState<number>(0);
+  const [choice, setChoice] = useState<RockPaperScissors>("Rock");
+  const [enemyResult, setEnemyResult] = useState<RockPaperScissors>("Rock");
+  const [playCount, setPlayCount] = useState<number>(0);
+  const [winCount, setWinCount] = useState<number>(0);
+  const [drawCount, setDrawCount] = useState<number>(0);
+  const [pastEnemyResults, setPastEnemyResults] = useState<RPSArray>([]);
+
+  const handleChooseRock = (): void => {
+    setBetAmount(betAmountInput);
+    setChoice("Rock");
+    setPlayCount((prev) => prev + 1);
+    const result: number = Math.floor(Math.random() * 3) + 1;
+    if (result === 3) {
+      setPoints((prev) => prev + betAmountInput * 1.95);
+      setWinCount((prev) => prev + 1);
+      setEnemyResult("Scissors");
+      setPastEnemyResults((prev) => [...prev, "S"]);
+    } else {
+      setPoints((prev) => prev - betAmountInput);
+      if (result === 1) {
+        setDrawCount((prev) => prev + 1);
+        setEnemyResult("Rock");
+        setPastEnemyResults((prev) => [...prev, "R"]);
+      } else {
+        setEnemyResult("Paper");
+        setPastEnemyResults((prev) => [...prev, "P"]);
+      }
+    }
+  };
+
+  const handleChoosePaper = (): void => {
+    setBetAmount(betAmountInput);
+    setChoice("Paper");
+    setPlayCount((prev) => prev + 1);
+    const result: number = Math.floor(Math.random() * 3) + 1;
+    if (result === 1) {
+      setPoints((prev) => prev + betAmountInput * 1.95);
+      setWinCount((prev) => prev + 1);
+      setEnemyResult("Rock");
+      setPastEnemyResults((prev) => [...prev, "R"]);
+    } else {
+      setPoints((prev) => prev - betAmountInput);
+      if (result === 2) {
+        setDrawCount((prev) => prev + 1);
+        setEnemyResult("Paper");
+        setPastEnemyResults((prev) => [...prev, "P"]);
+      } else {
+        setEnemyResult("Scissors");
+        setPastEnemyResults((prev) => [...prev, "S"]);
+      }
+    }
+  };
+
+  const handleChooseScissors = (): void => {
+    setBetAmount(betAmountInput);
+    setChoice("Scissors");
+    setPlayCount((prev) => prev + 1);
+    const result: number = Math.floor(Math.random() * 3) + 1;
+    if (result === 2) {
+      setPoints((prev) => prev + betAmountInput * 1.95);
+      setWinCount((prev) => prev + 1);
+      setEnemyResult("Paper");
+      setPastEnemyResults((prev) => [...prev, "P"]);
+    } else {
+      setPoints((prev) => prev - betAmountInput);
+      if (result === 1) {
+        setEnemyResult("Rock");
+        setPastEnemyResults((prev) => [...prev, "R"]);
+      } else {
+        setDrawCount((prev) => prev + 1);
+        setEnemyResult("Scissors");
+        setPastEnemyResults((prev) => [...prev, "S"]);
+      }
+    }
+  };
+
+  return (
+    <div data-testid={"RPS-Component"}>
+      <h4 data-testid={"Points"}>Points: {points}</h4>
+
+      <input
+        data-testid={"Bet-Amount"}
+        type="number"
+        min={100}
+        step={100}
+        required
+        value={betAmountInput}
+        onChange={(e) => {
+          const value: number = parseInt(e.target.value);
+          setBetAmountInput(value);
+        }}
+      />
+
+      <div className="rps-button-container">
+        <button
+          onClick={handleChooseRock}
+          disabled={
+            betAmountInput === 0 ||
+            betAmountInput % 100 !== 0 ||
+            betAmountInput > points
+          }
+        >
+          Rock
+        </button>
+        <button
+          onClick={handleChoosePaper}
+          disabled={
+            betAmountInput === 0 ||
+            betAmountInput % 100 !== 0 ||
+            betAmountInput > points
+          }
+        >
+          Paper
+        </button>
+        <button
+          onClick={handleChooseScissors}
+          disabled={
+            betAmountInput === 0 ||
+            betAmountInput % 100 !== 0 ||
+            betAmountInput > points
+          }
+        >
+          Scissors
+        </button>
+      </div>
+
+      {betAmount > 0 && (
+        <div>
+          <hr />
+
+          <p className="mb--10">
+            Your Choice:{" "}
+            {choice === "Rock" ? (
+              <span data-testid={"Choice"} className="red">
+                {choice}
+              </span>
+            ) : choice === "Paper" ? (
+              <span data-testid={"Choice"} className="blue">
+                {choice}
+              </span>
+            ) : (
+              <span data-testid={"Choice"} className="green">
+                {choice}
+              </span>
+            )}
+          </p>
+
+          <p className="mb--10">
+            Enemy Result:{" "}
+            {enemyResult === "Rock" ? (
+              <span data-testid={"Enemy-Result"} className="red">
+                {enemyResult}
+              </span>
+            ) : enemyResult === "Paper" ? (
+              <span data-testid={"Enemy-Result"} className="blue">
+                {enemyResult}
+              </span>
+            ) : (
+              <span data-testid={"Enemy-Result"} className="green">
+                {enemyResult}
+              </span>
+            )}
+          </p>
+
+          {(choice === "Rock" && enemyResult === "Scissors") ||
+          (choice === "Paper" && enemyResult === "Rock") ||
+          (choice === "Scissors" && enemyResult === "Paper") ? (
+            <p>Win! You earned {betAmount * 1.95} points!</p>
+          ) : choice === enemyResult ? (
+            <p>Draw! You lost {betAmount} points...</p>
+          ) : (
+            <p>Lose! You lost {betAmount} points...</p>
+          )}
+
+          <hr />
+
+          <p data-testid={"Play-Count"} className="mb--10">
+            Plays: {playCount}
+          </p>
+
+          <p data-testid={"Win-Count"} className="mb--10">
+            Wins: {winCount} ({((winCount / playCount) * 100).toFixed(2)}%)
+          </p>
+
+          <p data-testid={"Draw-Count"} className="mb--10">
+            Draws: {drawCount} ({((drawCount / playCount) * 100).toFixed(2)}%)
+          </p>
+
+          <p data-testid={"Loss-Count"}>
+            Losses: {playCount - winCount - drawCount} (
+            {(((playCount - winCount - drawCount) / playCount) * 100).toFixed(
+              2
+            )}
+            %)
+          </p>
+
+          <hr />
+
+          <p data-testid={"Past-Enemy-Results"}>
+            Past Enemy Results:{" "}
+            {pastEnemyResults.map((result, index) =>
+              result === "R" ? (
+                <span className="red" key={index}>
+                  {result}{" "}
+                </span>
+              ) : result === "P" ? (
+                <span className="blue" key={index}>
+                  {result}{" "}
+                </span>
+              ) : (
+                <span className="green" key={index}>
+                  {result}{" "}
+                </span>
+              )
+            )}
+          </p>
+
+          <p data-testid={"Rock-Count"} className="mb--10">
+            Rock: {pastEnemyResults.filter((result) => result === "R").length} (
+            {(
+              (pastEnemyResults.filter((result) => result === "R").length /
+                pastEnemyResults.length) *
+              100
+            ).toFixed(2)}
+            %)
+          </p>
+
+          <p data-testid={"Paper-Count"} className="mb--10">
+            Paper: {pastEnemyResults.filter((result) => result === "P").length}{" "}
+            (
+            {(
+              (pastEnemyResults.filter((result) => result === "P").length /
+                pastEnemyResults.length) *
+              100
+            ).toFixed(2)}
+            %)
+          </p>
+
+          <p data-testid={"Scissors-Count"}>
+            Scissors:{" "}
+            {pastEnemyResults.filter((result) => result === "S").length} (
+            {(
+              (pastEnemyResults.filter((result) => result === "S").length /
+                pastEnemyResults.length) *
+              100
+            ).toFixed(2)}
+            %)
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
