6c76753730e5122b82931f3674dca9386017eaed
Mokheon Park <mokimokheonpark@gmail.com>
2024-05-17 16:39:56 -0400

    integrated MongoDB

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+import { Db, Document, MongoClient, WithId } from "mongodb";
+import clientPromise from "@/lib/mongodb";
+
+export default async function Home() {
+  const client: MongoClient = await clientPromise;
+  const db: Db = client.db("Playground");
+  const test: WithId<Document> | null = await db
+    .collection("tests")
+    .findOne({ title: "test1" });
+
   return (
     <div className="pd-20">
       <h1>Welcome to Playground!</h1>
+      <p>{test?.title}</p>
+      <p>{test?.content}</p>
     </div>
   );
 }
