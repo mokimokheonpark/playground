596ffde7bb5ef57804cab0ab15213bcb54b13fa6
Mokheon Park <mokimokheonpark@gmail.com>
2024-05-22 20:16:29 -0400

    installed and set up Auth.js (NextAuth.js v5)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from "next";
 import Link from "next/link";
-import { Session, getServerSession } from "next-auth";
 import "./globals.css";
-import SignIn from "./components/SignIn";
-import SignOut from "./components/SignOut";
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 export const metadata: Metadata = {
   title: "Playground",
@@ -16,8 +12,6 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const session: Session | null = await getServerSession(authOptions);
-
   return (
     <html lang="en">
       <body>
@@ -26,7 +20,6 @@ export default async function RootLayout({
           <Link href="/coin">Coin</Link>
           <Link href="/dice">Dice</Link>
           <Link href="/rps">RPS</Link>
-          {/* {!session ? <SignIn /> : <SignOut />} */}
         </div>
         {children}
       </body>
