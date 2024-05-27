e61ceb3aec6aa9e130facd63e35739a29c3c72f4
Mokheon Park <mokimokheonpark@gmail.com>
2024-05-26 21:23:14 -0400

    did an additional test for SignIn component

--- app/components/SignIn.test.tsx
+++ app/components/SignIn.test.tsx
@@ -1,10 +1,20 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
+import { signIn } from "next-auth/react";
 import { SignIn } from "./SignIn";
 
-jest.mock("next-auth/react", () => ({}));
+jest.mock("next-auth/react", () => ({
+  signIn: jest.fn(),
+}));
 
 describe("SignIn Component", () => {
   test("renders without crashing", () => {
     render(<SignIn />);
+  });
+
+  test("calls signIn function when Sign In button is clicked", () => {
+    const { getByText } = render(<SignIn />);
+    const signInButton = getByText("Sign In");
+    fireEvent.click(signInButton);
+    expect(signIn).toHaveBeenCalledTimes(1);
   });
 });
