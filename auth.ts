aeccc12ac2c5a3d37b65d9cbde42267cf727885c
Mokheon Park <mokimokheonpark@gmail.com>
2024-05-31 18:12:47 -0400

    added credentials provider

--- auth.ts
+++ auth.ts
@@ -1,6 +1,28 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import GitHub from "next-auth/providers/github";
+import type { Db, Document, MongoClient, WithId } from "mongodb";
+import clientPromise from "@/lib/mongodb";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub],
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: "email" },
+        password: { label: "password", type: "password" },
+      },
+      authorize: async (credentials): Promise<any> => {
+        const client: MongoClient = await clientPromise;
+        const db: Db = client.db("Playground");
+        const user: WithId<Document> | null = await db
+          .collection("user")
+          .findOne({ email: credentials.email });
+        if (!user) {
+          throw new Error("Wrong Email.");
+        }
+        return user;
+      },
+    }),
+    GitHub,
+  ],
 });
